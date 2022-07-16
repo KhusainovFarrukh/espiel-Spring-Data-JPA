00112b74d9d6b11dd20472039a211917b135668f
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-16 14:55:32 +0500

    Initial commit

---
+++ src/main/java/kh/farrukh/espielspringdatajpa/EspielSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package kh.farrukh.espielspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspielSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspielSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kh/farrukh/espielspringdatajpa/EspielSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.farrukh.espielspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspielSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
