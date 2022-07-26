444e2682e4c447da5c2c5314f72142019eb8a4b1
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-26 15:42:20 +0500

    Added unidirectional Many-to-Many relationship

--- src/main/java/kh/farrukh/espielspringdatajpa/relationships/course/Course.java
+++ src/main/java/kh/farrukh/espielspringdatajpa/relationships/course/Course.java
@@ -25,6 +25,26 @@ public class Course {
     @Column(columnDefinition = "TEXT")
     private String description;
 
+    /**
+     * Every many-to-many association has two sides, the owning side
+     * and the non-owning, or inverse, side.  The join table is specified
+     * on the owning side. If the association is bidirectional, either
+     * side may be designated as the owning side.  If the relationship is
+     * bidirectional, the non-owning side must use the <code>mappedBy</code> element of
+     * the <code>ManyToMany</code> annotation to specify the relationship field or
+     * property of the owning side.
+     * <p>
+     * targetEntity -(Optional) The entity class that is the target of the
+     * association. Optional only if the collection-valued
+     * relationship property is defined using Java generics.  Must be
+     * specified otherwise. Defaults to the parameterized type of
+     * the collection when defined using generics.
+     *
+     * mappedBy - The field that owns the relationship. Required unless the relationship is unidirectional.
+     *
+     * todo more about cascade
+     * todo more about fetch
+     */
     // TODO: 7/26/22 more about FetchType
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Book> books;
